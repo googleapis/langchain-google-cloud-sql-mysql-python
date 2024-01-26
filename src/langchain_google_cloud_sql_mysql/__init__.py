53bfa4f56fba2273ae61801cf0b47f0b5b21ea19
jackwotherspoon <jackwoth@google.com>
2024-01-26 14:41:03 +0000

    chore: update file and folder naming

--- src/langchain_google_cloud_sql_mysql/__init__.py
+++ src/langchain_google_cloud_sql_mysql/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from langchain_google_cloud_sql_mysql.doc_loader import MySQLLoader
+from langchain_google_cloud_sql_mysql.mysql_loader import MySQLLoader
 from langchain_google_cloud_sql_mysql.mysql_engine import MySQLEngine
 
 __all__ = ["MySQLEngine", "MySQLLoader"]
