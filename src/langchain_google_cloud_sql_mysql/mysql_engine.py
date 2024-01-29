8f1926c2c04a8a4204f431a50ff75952df533bc9
Jack Wotherspoon <jackwoth@google.com>
2024-01-29 17:07:29 -0500

    chore: add whitespace
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>

--- src/langchain_google_cloud_sql_mysql/mysql_engine.py
+++ src/langchain_google_cloud_sql_mysql/mysql_engine.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # TODO: Remove below import when minimum supported Python version is 3.10
 from __future__ import annotations
 
