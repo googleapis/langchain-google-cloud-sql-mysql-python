49f6018f92a140340fae9139f22e7c6244c22fac
Liang <154559835+loeng2023@users.noreply.github.com>
2024-02-12 13:10:50 -0800

    feat: Add document saver class and support save/load/delete user journey (#16)

--- src/langchain_google_cloud_sql_mysql/mysql_loader.py
+++ src/langchain_google_cloud_sql_mysql/mysql_loader.py
@@ -13,42 +13,48 @@
 # limitations under the License.
 import json
 from collections.abc import Iterable
-from typing import Any, Dict, List, Optional, Sequence, cast
+from typing import Any, Dict, Iterator, List, Optional, Sequence, cast
 
+import pymysql
 import sqlalchemy
 from langchain_community.document_loaders.base import BaseLoader
 from langchain_core.documents import Document
 
 from langchain_google_cloud_sql_mysql.mysql_engine import MySQLEngine
 
+DEFAULT_CONTENT_COL = "page_content"
 DEFAULT_METADATA_COL = "langchain_metadata"
 
 
-def _parse_doc_from_table(
-    content_columns: Iterable[str],
-    metadata_columns: Iterable[str],
-    column_names: Iterable[str],
-    rows: Sequence[Any],
-) -> List[Document]:
-    docs = []
-    for row in rows:
-        page_content = " ".join(
-            str(getattr(row, column))
-            for column in content_columns
-            if column in column_names
-        )
-        metadata = {
-            column: getattr(row, column)
-            for column in metadata_columns
-            if column in column_names
-        }
-        if DEFAULT_METADATA_COL in metadata:
-            extra_metadata = json.loads(metadata[DEFAULT_METADATA_COL])
-            del metadata[DEFAULT_METADATA_COL]
-            metadata |= extra_metadata
-        doc = Document(page_content=page_content, metadata=metadata)
-        docs.append(doc)
-    return docs
+def _parse_doc_from_row(
+    content_columns: Iterable[str], metadata_columns: Iterable[str], row: Dict
+) -> Document:
+    page_content = " ".join(
+        str(row[column]) for column in content_columns if column in row
+    )
+    metadata: Dict[str, Any] = {}
+    # unnest metadata from langchain_metadata column
+    if DEFAULT_METADATA_COL in metadata_columns and row.get(DEFAULT_METADATA_COL):
+        for k, v in row[DEFAULT_METADATA_COL].items():
+            metadata[k] = v
+    # load metadata from other columns
+    for column in metadata_columns:
+        if column in row and column != DEFAULT_METADATA_COL:
+            metadata[column] = row[column]
+    return Document(page_content=page_content, metadata=metadata)
+
+
+def _parse_row_from_doc(column_names: Iterable[str], doc: Document) -> Dict:
+    doc_metadata = doc.metadata.copy()
+    row: Dict[str, Any] = {DEFAULT_CONTENT_COL: doc.page_content}
+    for entry in doc.metadata:
+        if entry in column_names:
+            row[entry] = doc_metadata[entry]
+            del doc_metadata[entry]
+    # store extra metadata in langchain_metadata column in json format
+    if DEFAULT_METADATA_COL in column_names and len(doc_metadata) > 0:
+        row[DEFAULT_METADATA_COL] = doc_metadata
+    return row
 
 
 class MySQLLoader(BaseLoader):
@@ -57,29 +63,13 @@ class MySQLLoader(BaseLoader):
     def __init__(
         self,
         engine: MySQLEngine,
-        query: str,
+        table_name: str = "",
+        query: str = "",
         content_columns: Optional[List[str]] = None,
         metadata_columns: Optional[List[str]] = None,
     ):
         """
-        Args:
-          engine (MySQLEngine): MySQLEngine object to connect to the MySQL database.
-          query (str): The query to execute in MySQL format.
-          content_columns (List[str]): The columns to write into the `page_content`
-             of the document. Optional.
-          metadata_columns (List[str]): The columns to write into the `metadata` of the document.
-             Optional.
-        """
-        self.engine = engine
-        self.query = query
-        self.content_columns = content_columns
-        self.metadata_columns = metadata_columns
-
-    def load(self) -> List[Document]:
-        """
-        Load langchain documents from a Cloud SQL MySQL database.
-
-        Document page content defaults to the first columns present in the query or table and
+        Document page content defaults to the first column present in the query or table and
         metadata defaults to all other columns. Use with content_columns to overwrite the column
         used for page content. Use metadata_columns to select specific metadata columns rather
         than using all remaining columns.
@@ -87,21 +77,149 @@ class MySQLLoader(BaseLoader):
         If multiple content columns are specified, page_content’s string format will default to
         space-separated string concatenation.
 
+        Args:
+          engine (MySQLEngine): MySQLEngine object to connect to the MySQL database.
+          table_name (str): The MySQL database table name. (OneOf: table_name, query).
+          query (str): The query to execute in MySQL format.  (OneOf: table_name, query).
+          content_columns (List[str]): The columns to write into the `page_content`
+             of the document. Optional.
+          metadata_columns (List[str]): The columns to write into the `metadata` of the document.
+             Optional.
+        """
+        self.engine = engine
+        self.table_name = table_name
+        self.query = query
+        self.content_columns = content_columns
+        self.metadata_columns = metadata_columns
+        if not self.table_name and not self.query:
+            raise ValueError("One of 'table_name' or 'query' must be specified.")
+        if self.table_name and self.query:
+            raise ValueError(
+                "Cannot specify both 'table_name' and 'query'. Specify 'table_name' to load "
+                "entire table or 'query' to load a specific query."
+            )
+
+    def load(self) -> List[Document]:
+        """
+        Load langchain documents from a Cloud SQL MySQL database.
+
         Returns:
             (List[langchain_core.documents.Document]): a list of Documents with metadata from
                 specific columns.
         """
+        return list(self.lazy_load())
+
+    def lazy_load(self) -> Iterator[Document]:
+        """
+        Lazy Load langchain documents from a Cloud SQL MySQL database. Use lazy load to avoid
+        caching all documents in memory at once.
+
+        Returns:
+            (Iterator[langchain_core.documents.Document]): a list of Documents with metadata from
+                specific columns.
+        """
+        if self.query:
+            stmt = sqlalchemy.text(self.query)
+        else:
+            stmt = sqlalchemy.text(f"select * from `{self.table_name}`;")
         with self.engine.connect() as connection:
-            result_proxy = connection.execute(sqlalchemy.text(self.query))
+            result_proxy = connection.execute(stmt)
+            # Get field type information.
+            # cursor.description is a sequence of 7-item sequences.
+            # Each of these sequences contains information describing one result column:
+            # - name, type_code, display_size, internal_size, precision, scale, null_ok
+            # The first two items (name and type_code) are mandatory, the other five are optional
+            # and are set to None if no meaningful values can be provided.
+            # link: https://peps.python.org/pep-0249/#description
+            column_types = [
+                cast(tuple, field)[0:2] for field in result_proxy.cursor.description
+            ]
             column_names = list(result_proxy.keys())
-            results = result_proxy.fetchall()
             content_columns = self.content_columns or [column_names[0]]
             metadata_columns = self.metadata_columns or [
                 col for col in column_names if col not in content_columns
             ]
-            return _parse_doc_from_table(
-                content_columns,
-                metadata_columns,
-                column_names,
-                results,
+            while True:
+                row = result_proxy.fetchone()
+                if not row:
+                    break
+                # Handle JSON fields
+                row_data = {}
+                for column, field_type in column_types:
+                    value = getattr(row, column)
+                    if field_type == pymysql.constants.FIELD_TYPE.JSON:
+                        row_data[column] = json.loads(value)
+                    else:
+                        row_data[column] = value
+                yield _parse_doc_from_row(content_columns, metadata_columns, row_data)
+
+
+class MySQLDocumentSaver:
+    """A class for saving langchain documents into a Cloud SQL MySQL database table."""
+
+    def __init__(
+        self,
+        engine: MySQLEngine,
+        table_name: str,
+    ):
+        """
+        MySQLDocumentSaver allows for saving of langchain documents in a database. If the table
+        doesn't exists, a table with default schema will be created. The default schema:
+            - page_content (type: text)
+            - langchain_metadata (type: JSON)
+
+        Args:
+          engine: MySQLEngine object to connect to the MySQL database.
+          table_name: The name of table for saving documents.
+        """
+        self.engine = engine
+        self.table_name = table_name
+        self._table = self.engine._load_document_table(table_name)
+        if DEFAULT_CONTENT_COL not in self._table.columns.keys():
+            raise ValueError(
+                f"Missing '{DEFAULT_CONTENT_COL}' field in table {table_name}."
             )
+
+    def add_documents(self, docs: List[Document]) -> None:
+        """
+        Save documents in the DocumentSaver table. Document’s metadata is added to columns if found or
+        stored in langchain_metadata JSON column.
+
+        Args:
+            docs (List[langchain_core.documents.Document]): a list of documents to be saved.
+        """
+        with self.engine.connect() as conn:
+            for doc in docs:
+                row = _parse_row_from_doc(self._table.columns.keys(), doc)
+                conn.execute(sqlalchemy.insert(self._table).values(row))
+            conn.commit()
+
+    def delete(self, docs: List[Document]) -> None:
+        """
+        Delete all instances of a document from the DocumentSaver table by matching the entire Document
+        object.
+
+        Args:
+            docs (List[langchain_core.documents.Document]): a list of documents to be deleted.
+        """
+        with self.engine.connect() as conn:
+            for doc in docs:
+                row = _parse_row_from_doc(self._table.columns.keys(), doc)
+                # delete by matching all fields of document
+                where_conditions = []
+                for col in self._table.columns:
+                    if str(col.type) == "JSON":
+                        where_conditions.append(
+                            sqlalchemy.func.json_contains(
+                                col,
+                                json.dumps(row[col.name]),
+                            )
+                        )
+                    else:
+                        where_conditions.append(col == row[col.name])
+                conn.execute(
+                    sqlalchemy.delete(self._table).where(
+                        sqlalchemy.and_(*where_conditions)
+                    )
+                )
+            conn.commit()
--- tests/integration/test_mysql_loader.py
+++ tests/integration/test_mysql_loader.py
@@ -17,9 +17,14 @@ from typing import Generator
 
 import pytest
 import sqlalchemy
+from langchain.text_splitter import CharacterTextSplitter
 from langchain_core.documents import Document
 
-from langchain_google_cloud_sql_mysql import MySQLEngine, MySQLLoader
+from langchain_google_cloud_sql_mysql import (
+    MySQLDocumentSaver,
+    MySQLEngine,
+    MySQLLoader,
+)
 
 project_id = os.environ["PROJECT_ID"]
 region = os.environ["REGION"]
@@ -219,7 +224,7 @@ def test_load_from_query_with_langchain_metadata(engine):
                 CREATE TABLE IF NOT EXISTS `{table_name}`(
                     fruit_id INT AUTO_INCREMENT PRIMARY KEY,
                     fruit_name VARCHAR(100) NOT NULL,
-                    variety VARCHAR(50),  
+                    variety VARCHAR(50),
                     quantity_in_stock INT NOT NULL,
                     price_per_unit DECIMAL(6,2) NOT NULL,
                     langchain_metadata JSON NOT NULL
@@ -258,3 +263,265 @@ def test_load_from_query_with_langchain_metadata(engine):
             },
         )
     ]
+
+
+def test_save_doc_with_default_metadata(engine):
+    engine.init_document_table(table_name)
+    test_docs = [
+        Document(
+            page_content="Apple Granny Smith 150 0.99 1",
+            metadata={"fruit_id": 1},
+        ),
+        Document(
+            page_content="Banana Cavendish 200 0.59 0",
+            metadata={"fruit_id": 2},
+        ),
+        Document(
+            page_content="Orange Navel 80 1.29 1",
+            metadata={"fruit_id": 3},
+        ),
+    ]
+    saver = MySQLDocumentSaver(engine=engine, table_name=table_name)
+    loader = MySQLLoader(engine=engine, table_name=table_name)
+
+    saver.add_documents(test_docs)
+    docs = loader.load()
+
+    assert docs == test_docs
+    assert engine._load_document_table(table_name).columns.keys() == [
+        "page_content",
+        "langchain_metadata",
+    ]
+
+
+@pytest.mark.parametrize("store_metadata", [True, False])
+def test_save_doc_with_customized_metadata(engine, store_metadata):
+    engine.init_document_table(
+        table_name,
+        metadata_columns=[
+            sqlalchemy.Column(
+                "fruit_name",
+                sqlalchemy.UnicodeText,
+                primary_key=False,
+                nullable=True,
+            ),
+            sqlalchemy.Column(
+                "organic",
+                sqlalchemy.Boolean,
+                primary_key=False,
+                nullable=True,
+            ),
+        ],
+        store_metadata=store_metadata,
+    )
+    test_docs = [
+        Document(
+            page_content="Granny Smith 150 0.99",
+            metadata={"fruit_id": 1, "fruit_name": "Apple", "organic": 1},
+        ),
+    ]
+    saver = MySQLDocumentSaver(engine=engine, table_name=table_name)
+    loader = MySQLLoader(
+        engine=engine,
+        table_name=table_name,
+        metadata_columns=[
+            "fruit_id",
+            "fruit_name",
+            "organic",
+        ],
+    )
+
+    saver.add_documents(test_docs)
+    docs = loader.load()
+
+    if store_metadata:
+        docs == test_docs
+        assert engine._load_document_table(table_name).columns.keys() == [
+            "page_content",
+            "fruit_name",
+            "organic",
+            "langchain_metadata",
+        ]
+    else:
+        assert docs == [
+            Document(
+                page_content="Granny Smith 150 0.99",
+                metadata={"fruit_name": "Apple", "organic": 1},
+            ),
+        ]
+        assert engine._load_document_table(table_name).columns.keys() == [
+            "page_content",
+            "fruit_name",
+            "organic",
+        ]
+
+
+def test_save_doc_without_metadata(engine):
+    engine.init_document_table(
+        table_name,
+        store_metadata=False,
+    )
+    test_docs = [
+        Document(
+            page_content="Granny Smith 150 0.99",
+            metadata={"fruit_id": 1, "fruit_name": "Apple", "organic": 1},
+        ),
+    ]
+    saver = MySQLDocumentSaver(engine=engine, table_name=table_name)
+    loader = MySQLLoader(
+        engine=engine,
+        table_name=table_name,
+    )
+
+    saver.add_documents(test_docs)
+    docs = loader.load()
+
+    assert docs == [
+        Document(
+            page_content="Granny Smith 150 0.99",
+            metadata={},
+        ),
+    ]
+    assert engine._load_document_table(table_name).columns.keys() == [
+        "page_content",
+    ]
+
+
+def test_delete_doc_with_default_metadata(engine):
+    engine.init_document_table(table_name)
+    test_docs = [
+        Document(
+            page_content="Apple Granny Smith 150 0.99 1",
+            metadata={"fruit_id": 1},
+        ),
+        Document(
+            page_content="Banana Cavendish 200 0.59 0 1",
+            metadata={"fruit_id": 2},
+        ),
+    ]
+    saver = MySQLDocumentSaver(engine=engine, table_name=table_name)
+    loader = MySQLLoader(engine=engine, table_name=table_name)
+
+    saver.add_documents(test_docs)
+    docs = loader.load()
+    assert docs == test_docs
+
+    saver.delete(docs[:1])
+    assert len(loader.load()) == 1
+
+    saver.delete(docs)
+    assert len(loader.load()) == 0
+
+
+@pytest.mark.parametrize("store_metadata", [True, False])
+def test_delete_doc_with_customized_metadata(engine, store_metadata):
+    engine.init_document_table(
+        table_name,
+        metadata_columns=[
+            sqlalchemy.Column(
+                "fruit_name",
+                sqlalchemy.UnicodeText,
+                primary_key=False,
+                nullable=True,
+            ),
+            sqlalchemy.Column(
+                "organic",
+                sqlalchemy.Boolean,
+                primary_key=False,
+                nullable=True,
+            ),
+        ],
+        store_metadata=store_metadata,
+    )
+    test_docs = [
+        Document(
+            page_content="Granny Smith 150 0.99",
+            metadata={"fruit-id": 1, "fruit_name": "Apple", "organic": 1},
+        ),
+        Document(
+            page_content="Cavendish 200 0.59 0",
+            metadata={"fruit_id": 2, "fruit_name": "Banana", "organic": 1},
+        ),
+    ]
+    saver = MySQLDocumentSaver(engine=engine, table_name=table_name)
+    loader = MySQLLoader(engine=engine, table_name=table_name)
+
+    saver.add_documents(test_docs)
+    docs = loader.load()
+    assert len(docs) == 2
+
+    saver.delete(docs[:1])
+    assert len(loader.load()) == 1
+
+    saver.delete(docs)
+    assert len(loader.load()) == 0
+
+
+def test_delete_doc_with_query(engine):
+    engine.init_document_table(
+        table_name,
+        metadata_columns=[
+            sqlalchemy.Column(
+                "fruit_name",
+                sqlalchemy.UnicodeText,
+                primary_key=False,
+                nullable=True,
+            ),
+            sqlalchemy.Column(
+                "organic",
+                sqlalchemy.Boolean,
+                primary_key=False,
+                nullable=True,
+            ),
+        ],
+        store_metadata=True,
+    )
+    test_docs = [
+        Document(
+            page_content="Granny Smith 150 0.99",
+            metadata={"fruit-id": 1, "fruit_name": "Apple", "organic": 1},
+        ),
+        Document(
+            page_content="Cavendish 200 0.59 0",
+            metadata={"fruit_id": 2, "fruit_name": "Banana", "organic": 0},
+        ),
+        Document(
+            page_content="Navel 80 1.29 1",
+            metadata={"fruit_id": 3, "fruit_name": "Orange", "organic": 1},
+        ),
+    ]
+    saver = MySQLDocumentSaver(engine=engine, table_name=table_name)
+    loader = MySQLLoader(engine=engine, table_name=table_name)
+    query = f"select * from `{table_name}` where fruit_name='Apple';"
+    query_loader = MySQLLoader(engine=engine, query=query)
+
+    saver.add_documents(test_docs)
+    docs = query_loader.load()
+    assert len(docs) == 1
+
+    saver.delete(docs)
+    assert len(loader.load()) == 2
+
+
+def test_load_and_spilt(engine):
+    engine.init_document_table(table_name)
+    text_splitter = CharacterTextSplitter(
+        separator=" ",
+        chunk_size=10,
+        chunk_overlap=2,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    test_docs = [
+        Document(
+            page_content="Apple Granny Smith 150 0.99 1",
+            metadata={"fruit_id": 1},
+        ),
+    ]
+    saver = MySQLDocumentSaver(engine=engine, table_name=table_name)
+    loader = MySQLLoader(engine=engine, table_name=table_name)
+
+    saver.add_documents(test_docs)
+    docs = loader.load_and_split(text_splitter=text_splitter)
+
+    assert len(docs) == 4
