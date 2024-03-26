dd9368d82fb7c9c58d2767363035163aa0ac96b3
jackwotherspoon <jackwoth@google.com>
2024-03-26 22:28:26 +0000

    feat: support adding and deleting from MySQLVectorStore

--- tests/integration/test_mysql_vectorstore.py
+++ tests/integration/test_mysql_vectorstore.py
@@ -146,12 +146,29 @@ class TestVectorStore:
         assert len(results) == 3
         engine._execute(f"TRUNCATE TABLE `{DEFAULT_TABLE}`")
 
+    def test_add_docs(self, engine, vs):
+        ids = [str(uuid.uuid4()) for i in range(len(texts))]
+        vs.add_documents(docs, ids=ids)
+        results = engine._fetch(f"SELECT * FROM `{DEFAULT_TABLE}`")
+        assert len(results) == 3
+        engine._execute(f"TRUNCATE TABLE `{DEFAULT_TABLE}`")
+
     def test_add_embedding(self, engine, vs):
         ids = [str(uuid.uuid4()) for _ in range(len(texts))]
         vs._add_embeddings(texts, embeddings, metadatas, ids)
         results = engine._fetch(f"SELECT * FROM `{DEFAULT_TABLE}`")
         assert len(results) == 3
         engine._execute(f"TRUNCATE TABLE `{DEFAULT_TABLE}`")
+
+    def test_delete(self, engine, vs):
+        ids = [str(uuid.uuid4()) for _ in range(len(texts))]
+        vs.add_texts(texts, ids=ids)
+        results = engine._fetch(f"SELECT * FROM `{DEFAULT_TABLE}`")
+        assert len(results) == 3
+        # delete an ID
+        vs.delete([ids[0]])
+        results = engine._fetch(f"SELECT * FROM `{DEFAULT_TABLE}`")
+        assert len(results) == 2
 
     def test_add_texts_custom(self, engine, vs_custom):
         ids = [str(uuid.uuid4()) for _ in range(len(texts))]
@@ -172,11 +189,50 @@ class TestVectorStore:
         assert len(results) == 6
         engine._execute(f"TRUNCATE TABLE `{CUSTOM_TABLE}`")
 
+    def test_add_docs_custom(self, engine, vs_custom):
+        ids = [str(uuid.uuid4()) for i in range(len(texts))]
+        docs = [
+            Document(
+                page_content=texts[i],
+                metadata={"page": str(i), "source": "google.com"},
+            )
+            for i in range(len(texts))
+        ]
+        vs_custom.add_documents(docs, ids=ids)
+
+        results = engine._fetch(f"SELECT * FROM `{CUSTOM_TABLE}`")
+        content = [result["mycontent"] for result in results]
+        assert len(results) == 3
+        assert "foo" in content
+        assert "bar" in content
+        assert "baz" in content
+        assert results[0]["myembedding"]
+        pages = [result["page"] for result in results]
+        assert "0" in pages
+        assert "1" in pages
+        assert "2" in pages
+        assert results[0]["source"] == "google.com"
+        engine._execute(f"TRUNCATE TABLE `{CUSTOM_TABLE}`")
+
     def test_add_embedding_custom(self, engine, vs_custom):
         ids = [str(uuid.uuid4()) for _ in range(len(texts))]
         vs_custom._add_embeddings(texts, embeddings, metadatas, ids)
         results = engine._fetch(f"SELECT * FROM `{CUSTOM_TABLE}`")
         assert len(results) == 3
         engine._execute(f"TRUNCATE TABLE `{CUSTOM_TABLE}`")
+
+    def test_delete_custom(self, engine, vs_custom):
+        ids = [str(uuid.uuid4()) for _ in range(len(texts))]
+        vs_custom.add_texts(texts, ids=ids)
+        results = engine._fetch(f"SELECT * FROM `{CUSTOM_TABLE}`")
+        content = [result["mycontent"] for result in results]
+        assert len(results) == 3
+        assert "foo" in content
+        # delete an ID
+        vs_custom.delete([ids[0]])
+        results = engine._fetch(f"SELECT * FROM `{CUSTOM_TABLE}`")
+        content = [result["mycontent"] for result in results]
+        assert len(results) == 2
+        assert "foo" not in content
 
     # Need tests for store metadata=False
