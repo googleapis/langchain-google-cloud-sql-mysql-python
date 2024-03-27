436093803522918a055d6d9e06ec25df98e36d1c
totoleon <hailong.leon@gmail.com>
2024-03-27 14:48:25 -0700

    adding docstrings

--- src/langchain_google_cloud_sql_mysql/indexes.py
+++ src/langchain_google_cloud_sql_mysql/indexes.py
@@ -1,38 +1,33 @@
-# Copyright 2024 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
 
-
 class SearchType(Enum):
-    """Enum for search types."""
+    """Defines the types of search algorithms that can be used.
 
+    Attributes:
+        KNN: K-Nearest Neighbors search.
+        ANN: Approximate Nearest Neighbors search.
+    """
     KNN = "KNN"
     ANN = "ANN"
 
-
 @dataclass
 class QueryOptions:
+    """Holds configuration options for executing a search query.
+
+    Attributes:
+        num_partitions (Optional[int]): The number of partitions to divide the search space into. None means default partitioning.
+        num_neighbors (Optional[int]): The number of nearest neighbors to retrieve. None means use the default.
+        search_type (SearchType): The type of search algorithm to use. Defaults to KNN.
+    """
+
     num_partitions: Optional[int] = None
     num_neighbors: Optional[int] = None
     search_type: SearchType = SearchType.KNN
 
     def to_string(self) -> str:
-        """
-        Custom string representation of the QueryOptions instance.
-        """
+        """Generates a custom string representation of the QueryOptions instance."""
         return str(
             {
                 "num_partitions": self.num_partitions,
@@ -41,28 +36,42 @@ class QueryOptions:
             }
         )
 
-
 DEFAULT_QUERY_OPTIONS = QueryOptions()
 
-
 class IndexType(Enum):
-    """Enum for index types."""
+    """Defines the types of indexes that can be used for vector storage.
 
+    Attributes:
+        BRUTE_FORCE_SCAN: A simple brute force scan approach.
+        TREE_AH: A tree-based index, specifically Annoy (Approximate Nearest Neighbors Oh Yeah).
+        TREE_SQ: A tree-based index, specifically ScaNN (Scalable Nearest Neighbors).
+    """
     BRUTE_FORCE_SCAN = "BRUTE_FORCE"
     TREE_AH = "TREE_AH"
     TREE_SQ = "TREE_SQ"
 
-
 class DistanceMeasure(Enum):
-    """Enum for distance measures."""
+    """Enumerates the types of distance measures that can be used in searches.
 
+    Attributes:
+        COSINE: Cosine similarity measure.
+        SQUARED_L2: Squared L2 norm (Euclidean) distance.
+        DOT_PRODUCT: Dot product similarity.
+    """
     COSINE = "cosine"
     SQUARED_L2 = "squared_l2"
     DOT_PRODUCT = "dot_product"
 
-
 class VectorIndex:
-    """VectorIndex class for index types."""
+    """Represents a vector index for storing and querying vectors.
+
+    Attributes:
+        name (Optional[str]): The name of the index.
+        index_type (Optional[IndexType]): The type of index.
+        distance_measure (Optional[DistanceMeasure]): The distance measure to use for the index.
+        num_partitions (Optional[int]): The number of partitions for the index. None for default.
+        num_neighbors (Optional[int]): The default number of neighbors to return for queries.
+    """
 
     def __init__(
         self,
@@ -72,6 +81,7 @@ class VectorIndex:
         num_partitions: Optional[int] = None,
         num_neighbors: Optional[int] = None,
     ):
+        """Initializes a new instance of the VectorIndex class."""
         self.name = name
         self.index_type = index_type
         self.distance_measure = distance_measure
